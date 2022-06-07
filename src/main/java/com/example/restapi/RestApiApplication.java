1844cbb7be6a1f67d1f63e17374d2fd2b30a42d1
Maxim <MSergeyev@beeline.kz>
2022-06-07 22:03:44 +0600

    DUMP bd

--- src/main/java/com/example/restapi/RestApiApplication.java
+++ src/main/java/com/example/restapi/RestApiApplication.java
@@ -1,11 +1,5 @@
 package com.example.restapi;
 
-import com.example.restapi.model.Customers;
-import com.example.restapi.model.Operation;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
